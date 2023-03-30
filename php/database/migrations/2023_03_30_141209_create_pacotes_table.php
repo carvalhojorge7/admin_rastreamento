fdd5a2a370cae0723140c2a5253156c828146064
Jorge Carvalho <jorge@softbuilder.com.br>
2023-03-30 14:16:40 -0300

    add migrates

--- php/database/migrations/2023_03_30_141209_create_pacotes_table.php
+++ php/database/migrations/2023_03_30_141209_create_pacotes_table.php
@@ -3,9 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use phpDocumentor\Reflection\Types\Nullable;
 
-class CreateUsersTable extends Migration
+class CreatePacotesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('pacotes', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('pedido')->unique();
+            $table->string('cliente');
+            $table->string('status');
+            $table->string('previsao');
+            $table->string('detalhes')->default('Sem Informações detalhadas.');
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('pacotes');
     }
 }
